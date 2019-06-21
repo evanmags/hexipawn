16f0bf9f797825cf4d53c1244b8576e0e970edaf
emags112 <magnussen.evan@gmail.com>
2019-06-21 11:44:50 -0400

    modified init

--- hexipawn.py
+++ hexipawn.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-
+import re
+import os
 from modules.game_state import Game_State
 from modules.memory import memory
 from modules.moves import check_possible_moves
 
-def main():
-  game = init_game()
-  if game == None:
-    return
+def main(skipInit=False):
+  game = init_game(skipInit)
+  if game == None: return
   game.print_board()
   
   while True:
@@ -31,15 +31,18 @@ def main():
   game.save_memory([])
   return check_replay()
 
-def init_game():
-  
+def init_game(skip):
+  if skip: return Game_State()
+
+  os.system('clear')
+
   print("\nWELCOME TO HEXIPAWN\n")
-  print("Below you will see a board, when selecting pieces and moves\nuse the numbers in each square to select them.")
-  print(f" 1 | 2 | 3 ")
-  print(f"-----------")
-  print(f" 4 | 5 | 6 ")
-  print(f"-----------")
-  print(f" 7 | 8 | 9 \n")
+  print("Below you will see a board, when selecting pieces and moves\nuse the numbers in each square to select them.\n")
+  print(f"\t\t 1 | 2 | 3 ")
+  print(f"\t\t-----------")
+  print(f"\t\t 4 | 5 | 6 ")
+  print(f"\t\t-----------")
+  print(f"\t\t 7 | 8 | 9 \n")
 
   print("RULES:\t1. You can only move one space at a time.")
   print("\t2. You can only move diagonal to capture a piece.")
@@ -50,19 +53,17 @@ def init_game():
 
 
   print("ENJOY!!\n")
-  
-  if (input("Type Y to play or N to quit: ").upper() == "N"):
-    return print("sorry to see you go.")
+
+  if re.match("^(y|yes)$", input("Type Y to play or N to quit: "), flags=re.IGNORECASE):
+    return Game_State()
   else:
-    return Game_State.new()
+    return print("Sorry to see you go.")
 
 def check_replay():
-  if input("Would you like to play again?") == "Y":
-    return main()
-  
+  if re.match("^(y|yes)$", input("Would you like to play again?"), flags=re.IGNORECASE):
+    return main(True)
   else:
-    print("thanks for playing!")
-    return
+    return print("thanks for playing!")
 
 if __name__ == "__main__":
   main()
